b97e34c02fef05720ca465c4d2961a2ce6ffe59e
Duane McFarlane <duanemcfarlane@yahoo.com>
2018-07-18 22:58:23 -0400

    added jquery..refactored some code

--- main.js
+++ main.js
@@ -4,65 +4,34 @@
 // if user adds another item to the array, return the calculation and push new item into the array
 // once user clicks “=“, run calculation function
 
-let result;
-let numberKeys = [1,2,3,4,5,6,7,8,9,0];
-// let input = document.getElementById("screen").value;
-let numbers = [];
-numbers.push(document.getElementById("screen").value);
-numbers.forEach(function(element) {
-    console.log(element);
-    });
+// let result;
+// let numberKeys = [1,2,3,4,5,6,7,8,9,0];
+// // let input = document.getElementById("screen").value;
+// let numbers = [];
+// numbers.push(document.getElementById("screen").value);
+// numbers.forEach(function(element) {
+//     console.log(element);
+//     });
 
+let screen = document.querySelector("#screen");
+let equal = document.querySelector("#equal");
+let add = document.querySelector("#addition");
+let subtract = document.querySelector("#subtraction");
+let divide = document.querySelector("#division");
+let multiply = document.querySelector("#multiplication");
 
-document.querySelector("#one").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 1;
+// add click event to buttons 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+
+$(".integer").click(function(){
+    let text = $(this).text();
+    console.log(text);
 });
 
-document.querySelector("#two").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 2;
-});
+// $(".clear").click(function(){
+//     let text = $(this).text("");
+//     console.log(text);
+// });
 
-document.querySelector("#three").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 3;
-});
-
-document.querySelector("#four").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 4;
-});
-
-document.querySelector("#five").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 5;
-});
-
-document.querySelector("#six").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 6;
-});
-
-document.querySelector("#seven").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 7;
-});
-
-document.querySelector("#eight").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 8;
-});
-
-document.querySelector("#nine").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 9;
-});
-
-document.querySelector("#zero").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value += 0;
-});
 
 document.querySelector("#clear").addEventListener("click", function() {
     let text = document.querySelector("#screen");
