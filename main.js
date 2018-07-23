d8a3eeeb3d6fca26e337e3942110117ad253a199
Duane McFarlane <duanemcfarlane@yahoo.com>
2018-07-22 21:46:24 -0400

    added functionality

--- main.js
+++ main.js
@@ -1,39 +1,49 @@
-// Make an array that stores input
-// Push new item to the array after user clicks a math symbol (- + / *)
-// if user clicks “=", run the calculation function
-// if user adds another item to the array, return the calculation and push new item into the array
-// once user clicks “=“, run calculation function
-
-// let result;
-// let numberKeys = [1,2,3,4,5,6,7,8,9,0];
-// // let input = document.getElementById("screen").value;
-// let numbers = [];
-// numbers.push(document.getElementById("screen").value);
-// numbers.forEach(function(element) {
-//     console.log(element);
-//     });
-
-let screen = document.querySelector("#screen");
-let equal = document.querySelector("#equal");
-let add = document.querySelector("#addition");
-let subtract = document.querySelector("#subtraction");
-let divide = document.querySelector("#division");
-let multiply = document.querySelector("#multiplication");
-
-// add click event to buttons 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-
-$(".integer").click(function(){
-    let text = $(this).text();
-    console.log(text);
-});
-
-// $(".clear").click(function(){
-//     let text = $(this).text("");
-//     console.log(text);
-// });
-
-
-document.querySelector("#clear").addEventListener("click", function() {
-    let text = document.querySelector("#screen");
-    text.value = null;
-});
+$(document).ready(function() {
+    var input1 = 0;
+    var input2 = '0';
+    var operation = null;
+    var prevEntry = 0;
+    updateScreen(input1);
+    
+    $('.integer').on('click', function(evt) {
+      var buttonPressed = $(this).val();
+      console.log(buttonPressed);
+      
+   if (isNumber(buttonPressed)) {
+        if (input2 === '0') input2 = buttonPressed;
+        else input2 = input2 + buttonPressed;
+      } else if (isOperator(buttonPressed)) {
+        prevEntry = parseFloat(input2);
+        operation = buttonPressed;
+        input2 = '';
+      } else if (buttonPressed === '=') {
+        input2 = operate(prevEntry, input2, operation);
+        operation = null;
+      }
+      
+      updateScreen(input2);
+    });
+   });
+   
+   updateScreen = function(displayValue) {
+    var displayValue = displayValue.toString();
+    $('#screen').val(displayValue.substring(0));
+   };
+   
+   isNumber = function(value) {
+    return !isNaN(value);
+   }
+   
+   isOperator = function(value) {
+    return value === '/' || value === '*' || value === '+' || value === '-';
+   };
+   
+   operate = function(a, b, operation) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    console.log(a, b, operation);
+    if (operation === '+') return a + b;
+    if (operation === '-') return a - b;
+    if (operation === '*') return a * b;
+    if (operation === '/') return a / b;
+   }
